facc39685bc52ffc94bb81dc9291a146bcc9c05a
bartektenDev <bartek8991@live.com>
2019-01-21 18:39:23 -0600

    Second commit

---
+++ assets/dataload.js
@@ -0,0 +1,6 @@
+function loadDash() {
+ document.getElementById("contents").innerHTML = "Welcome to the Dashboard";
+}
+function loadPref() {
+ document.getElementById("contents").innerHTML = "Welcome to Preferences";
+}
