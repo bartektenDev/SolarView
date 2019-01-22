71a960f5db251747ad95b65152f0ed46e11ad4f2
bartektenDev <bartek8991@live.com>
2019-01-21 19:11:07 -0600

    Added tab content view

--- assets/dataload.js
+++ assets/dataload.js
@@ -1,6 +1,26 @@
 function loadDash() {
- document.getElementById("contents").innerHTML = "Welcome to the Dashboard";
+ document.getElementById("menuTitle").innerHTML = "Dashboard";
 }
 function loadPref() {
- document.getElementById("contents").innerHTML = "Welcome to Preferences";
+ document.getElementById("menuTitle").innerHTML = "Preferences";
+ readTextFile()
+}
+
+function readTextFile()
+{
+    var file = "file:///D:/GitProjects/solarpanelUIforEnecsys/assets/config.p";
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
 }
--- dataload.js
+++
@@ -1,6 +0,0 @@
-function loadDash() {
- document.getElementById("contents").innerHTML = "Welcome to the Dashboard";
-}
-function loadPref() {
- document.getElementById("contents").innerHTML = "Welcome to Preferences";
-}
